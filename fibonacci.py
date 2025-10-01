5e97b560a275cd760698e8449f7271ef3c6ca0b1
Duongy <duongy@uoguelph.ca>
2025-10-01 12:08:50 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
-# Fibonacci Sequence Exercise
+# fib Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
 # Validate that the input is a positive integer.
-# Use a for loop to print the Fibonacci sequence up to that many terms.
+# Use a for loop to print the fib sequence up to that many terms.
+
+fibOut = 0;
+fib1 = 0
+fib2 = 1;
+int input = 0;
+int output;
+
+while (input < 0):
+  input = input("Enter a number to know the fib sequence to it: ");
+  if (input < 0):
+    print("Please enter a positive number!\n");
+
+for x in range (input):
+  fibOut = fib1 + fib2;
+  print("fibOut\n");
+  fib2 = fib1;
+  fib1 = fibOut;
+
+  
